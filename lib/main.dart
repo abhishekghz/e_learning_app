c10b24081d4d1e7a04b60d0e50b609f7bd5fd907
abhishekghz <sunny7739094749@gmail.com>
2022-02-26 23:01:12 +0530

    Initial commit and Update the completed project.

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:e_learning_app/screens/welcome_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      //removing debug banner
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter ',
+      theme: ThemeData(
+
+        primarySwatch: Colors.blue,
+      ),
+      home: const WelcomeScreen(),
+    );
+  }
+}
